b32258718ebbfef9b8a79e40dd26ef1e446240e6
Aasai <aasaithambi57@gmail.com>
2024-04-28 20:50:10 +0530

    changing infra s3bucket name

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "dev-proj-1-remote-state-bucket-5793"
+    bucket = "dev-proj-1-remote-state-bucket-57"
     key    = "devops-project-1/terraform.tfstate"
     region = "us-east-1"
   }
