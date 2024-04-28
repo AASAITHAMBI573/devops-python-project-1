31776609f4ccca0fafe0625c5cd511990b719180
Aasai <aasaithambi57@gmail.com>
2024-04-28 22:14:53 +0530

    changing infra RDS Engine_version change

--- infra/rds/main.tf
+++ infra/rds/main.tf
@@ -16,8 +16,8 @@ resource "aws_db_instance" "default" {
   allocated_storage       = 10
   storage_type            = "gp2"
   engine                  = "mysql"
-  engine_version          = "8.0.35"
-  instance_class          = "db.r6g.large"
+  engine_version          = "5.7"
+  instance_class          = "db.t2.medium"
   identifier              = var.mysql_db_identifier
   username                = var.mysql_username
   password                = var.mysql_password
